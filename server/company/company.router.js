dcba87e46fb98956edb9096e9d5085b5029d3cc4
Nam Phan <sockyone>
2019-11-18 10:33:52 +0700

    cm

--- server/company/company.router.js
+++ server/company/company.router.js
@@ -1,6 +1,11 @@
 const express = require('express');
 let router = express.Router();
 let companyModel = require('./company.model');
+let models = require("../models-master/index");
+let User = models.User;
+let Company = models.Company;
+let ResponseJSON = require('../response');
+let userModel = require('./../user/user.model');
 
 router.post('/company/new', function (req, res) {
     companyModel.createCompany(req.body, function (status) {
@@ -29,6 +34,33 @@ router.post('/company/edit', function (req, res) {
 router.post('/company/list', function (req, res) {
     companyModel.listCompany(req.body, function (status) {
         res.send(status);
+    });
+});
+
+router.post('/company/users', (req, res)=>{
+    let company = req.decoded.company;
+    let role = req.decoded.role;
+
+    if (role == 2) {
+        //this is admin
+        userModel.listUser(req.body, function (status) {
+            res.send(status);
+        }, req.decoded);
+        return;
+    }
+
+    Company.findAll({where: {name: company}})
+    .then((rs)=>{
+        if (rs.length > 0) {
+            let idCompany = rs[0].idCompany;
+            User.findAll({where: {idCompany: idCompany}})
+            .then(usersInCompany=>{
+                //find All project with
+                res.json(ResponseJSON(200, 'Successfully', usersInCompany));
+            }); 
+        } else {
+            res.json(ResponseJSON(512, 'Failed', {}));
+        }
     });
 });
 
