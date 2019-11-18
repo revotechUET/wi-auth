a6f20e9761cf77b3fb42935c8fc2dde762ea12d7
Nam Phan <sockyone>
2019-11-18 09:54:31 +0700

    find user in company

--- server/company/company.router.js
+++ server/company/company.router.js
@@ -1,6 +1,10 @@
 const express = require('express');
 let router = express.Router();
 let companyModel = require('./company.model');
+let models = require("../models-master/index");
+let User = models.User;
+let Company = models.Company;
+let ResponseJSON = require('../response');
 
 router.post('/company/new', function (req, res) {
     companyModel.createCompany(req.body, function (status) {
@@ -30,6 +34,25 @@ router.post('/company/list', function (req, res) {
     companyModel.listCompany(req.body, function (status) {
         res.send(status);
     });
+});
+
+router.post('/company/users', (req, res)=>{
+    //this router return list user of your company
+    let company = req.decoded.company;
+    Company.findAll({where: {name: company}})
+    .then((rs)=>{
+        if (rs.length > 0) {
+            let idCompany = rs[0].idCompany;
+            User.findAll({where: {idCompany: idCompany}})
+            .then(usersInCompany=>{
+                res.json(ResponseJSON(200, 'Successfully', usersInCompany));
+            }); 
+        } else {
+            res.json(ResponseJSON(512, 'Failed', {}));
+        }
+    });
+    
+    
 });
 
 module.exports = router;
