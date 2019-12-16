add5ca5a826fb9e71f370bf5c6bfed6c12e9e575
Nam Phan <sockyone>
2019-12-16 15:31:38 +0700

    revert err

--- server/company/company.model.js
+++ server/company/company.model.js
@@ -60,13 +60,7 @@ function infoCompany(payload, callback) {
 
 function listCompany(payload, callback) {
     let resp = [];
-    let condition = {};
-    if (payload.decoded.role > 0) {
-        if (payload.decoded.company) {
-            condition.name = payload.decoded.company;
-        }
-    }
-    models.Company.findAll(condition).then(comps => {
+    models.Company.findAll().then(comps => {
         async.each(comps, function (comp, next) {
             comp = comp.toJSON();
             models.User.findAndCountAll({where: {idCompany: comp.idCompany, status: "Active"}}).then(users => {
