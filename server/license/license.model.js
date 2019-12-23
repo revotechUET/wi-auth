712c65be0c873cb03517a578ccbc488bcbd77c74
Quang LN <quangln2810@gmail.com>
2019-12-23 16:10:43 +0700

    update button perm

--- server/license/license.model.js
+++ server/license/license.model.js
@@ -14,6 +14,8 @@ function getUserLicense(payload, cb, username) {
         case "package":
             options.include = {model: LicensePackage};
             break;
+        case "button":
+            options.include = { model: LicensePackage, include: { model: Feature, include: { model: Api }, where: { type: 'BUTTON' } } };
         default:
             options.include = {model: LicensePackage, include: {model: Feature, include: {model: Api}}};
             break;
