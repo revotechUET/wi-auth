70bdbb8aef927828198f35a35440f04e52f746b5
Quang LN <quangln2810@gmail.com>
2019-12-26 15:32:39 +0700

    update

--- server/license/sync-feature-api.js
+++ server/license/sync-feature-api.js
@@ -6,17 +6,13 @@ module.exports = async function () {
     let rows = xlsx.getRows(file, 'feature_api').slice(2);
     for (let i = 0; i < rows.length; i++) {
         let row = rows[i];
-        let route = await Api.findOrCreate({
-            where: {
-                idI2gApi: row[0],
-                route: row[1],
-                type: row[2]
-            }, defaults: {
+        let route = await Api.upsert(
+            {
                 idI2gApi: row[0],
                 route: row[1],
                 type: row[2]
             }
-        });
+        );
         for (let i = 1; i <= 10; i++) {
             await route[0].addI2g_feature(i, {through: {perm: row[2 + i]}});
         }
