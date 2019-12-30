6056b299a4fa4ef161f34ee17e73054079d6ece3
Nam Phan <sockyone>
2019-12-30 10:53:20 +0700

    update

--- server/models-master/Company.js
+++ server/models-master/Company.js
@@ -28,6 +28,11 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING(250),
             allowNull: false,
             defaultValue: ""
+        },
+        shortname: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            unique: true
         }
     });
 };
