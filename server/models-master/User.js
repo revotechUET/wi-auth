530cc7d142a0069a8b0ec561b378065082df7fe2
NamPhan <sockyone16899@gmail.com>
2020-06-10 16:19:55 +0700

    done

--- server/models-master/User.js
+++ server/models-master/User.js
@@ -12,7 +12,7 @@ module.exports = function (sequelize, DataTypes) {
             unique: true,
             validate: {
                 containsSpecialCharacter(name) {
-                    console.log("Validating:", name);
+                    //console.log("Validating:", name);
                     if (/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(name)) throw new Error("Username can not contain special character");
                 }
             }
