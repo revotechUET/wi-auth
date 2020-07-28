3fb7741919b36ae759b463d23b8ea167eddc99aa
Hoang Bui <bdhoang.ht@gmail.com>
2020-07-28 10:25:26 +0700

    fix

--- server/authenticate/logout.js
+++ server/authenticate/logout.js
@@ -7,13 +7,9 @@ let ResponseJSON = require('../response');
 router.post('/logout', (req, res) => {
     if (!req.body.client_id) return res.send(ResponseJSON(512, "Need client_id", "Need client_id in payload"));
     RefreshTokenModel.destroy({ where: { client_id: req.body.client_id }, truncate: true }).then(r => {
-        if (r) {
-            r.destroy().then(() => {
-                res.send(ResponseJSON(200, "Done", r));
-            })
-        } else {
-            res.send(ResponseJSON(512, "Session not found", "Session not found"));
-        }
+        res.send(ResponseJSON(200, "Done", r));
+    }).catch(e => {
+        res.send(ResponseJSON(512, "error", e));
     })
 });
 
