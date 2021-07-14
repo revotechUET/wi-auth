485520b539d9978c05718d4ae66ae9c8e1c24991
Hoang Bui <bdhoang.ht@gmail.com>
2021-07-14 17:31:43 +0700

    add sharing  notification feature

--- server/shared-project/shared-project.model.js
+++ server/shared-project/shared-project.model.js
@@ -2,20 +2,28 @@ let ResponseJSON = require('../response');
 let Model = require('../models-master');
 let asyncEach = require('async/each');
 let crypto = require('crypto');
-
+let fs = require('fs');
+let wiNotification = require('@revotechuet/wi-notification');
+let notice;
+if (process.env.AWS_SMTP_USER) {
+    notice = new wiNotification({
+        user: process.env.AWS_SMTP_USER,
+        password: process.env.AWS_SMTP_PASSWORD
+    });
+}
 function getRandomHash() {
-	const current_date = (new Date()).valueOf().toString();
-	const random = Math.random().toString();
-	return (crypto.createHash('sha1').update(current_date + random).digest('hex'));
+    const current_date = (new Date()).valueOf().toString();
+    const random = Math.random().toString();
+    return (crypto.createHash('sha1').update(current_date + random).digest('hex'));
 }
 
 function createNewSharedProject(data, done, username) {
     // let conditions = username ? {username: username} : {username: data.username};
-    let conditions = data.username ? {username: data.username} : {username: username};
-    Model.User.findOne({where: conditions}).then((user => {
+    let conditions = data.username ? { username: data.username } : { username: username };
+    Model.User.findOne({ where: conditions }).then((user => {
         data.shareKey = getRandomHash();
         Model.SharedProject.findOrCreate({
-            where: {project_name: data.name || data.project_name, idOwner: user.idUser},
+            where: { project_name: data.name || data.project_name, idOwner: user.idUser },
             defaults: {
                 project_name: data.name || data.project_name,
                 shareKey: data.shareKey,
@@ -30,9 +38,9 @@ function createNewSharedProject(data, done, username) {
 }
 
 function removeSharedProject(data, done) {
-    Model.User.findOne({where: {username: data.createdBy}}).then(user => {
+    Model.User.findOne({ where: { username: data.createdBy } }).then(user => {
         if (user) {
-            Model.SharedProject.findOne({where: {idOwner: user.idUser, project_name: data.name}}).then(p => {
+            Model.SharedProject.findOne({ where: { idOwner: user.idUser, project_name: data.name } }).then(p => {
                 if (p) {
                     p.destroy().then(() => {
                         done(ResponseJSON(200, "Done", p));
@@ -50,7 +58,7 @@ function removeSharedProject(data, done) {
 function addToGroup(data, done) {
     if (data.type === "add") {
         if (data.shareKey) {
-            Model.SharedProject.findOne({where: {shareKey: data.shareKey}}).then(rs => {
+            Model.SharedProject.findOne({ where: { shareKey: data.shareKey } }).then(rs => {
                 if (rs) {
                     let defaultPerm = require('../utils/default-permission.json');
                     if (data.idGroup === 0) {
@@ -73,13 +81,15 @@ function addToGroup(data, done) {
                                 }
                             }).then(user => {
                                 gr[0].addUser(user.idUser);
-                                rs.addGroup(gr[0].idGroup, {through: {permission: defaultPerm}});
+                                rs.addGroup(gr[0].idGroup, { through: { permission: defaultPerm } });
+                                doNotification(gr[0].idGroup, rs, "add");
                                 done(ResponseJSON(200, "Done", rs));
                             });
                         });
                     } else {
-                        rs.addGroup(data.idGroup, {through: {permission: defaultPerm}});
+                        rs.addGroup(data.idGroup, { through: { permission: defaultPerm } });
                         done(ResponseJSON(200, 'Successful', rs));
+                        doNotification(data.idGroup, rs, "add");
                     }
                 } else {
                     done(ResponseJSON(512, "Share key isn't correct"));
@@ -91,8 +101,9 @@ function addToGroup(data, done) {
         } else {
             Model.SharedProject.findByPk(data.idSharedProject).then(rs => {
                 let defaultPerm = require('../utils/default-permission.json');
-                rs.addGroup(data.idGroup, {through: {permission: defaultPerm}});
+                rs.addGroup(data.idGroup, { through: { permission: defaultPerm } });
                 done(ResponseJSON(200, 'Successful', data));
+                doNotification(data.idGroup, rs, "add");
             }).catch(err => {
                 done(ResponseJSON(512, err, err));
             });
@@ -100,22 +111,21 @@ function addToGroup(data, done) {
     } else if (data.type === "remove") {
         Model.SharedProject.findByPk(data.idSharedProject).then(rs => {
             rs.removeGroup(data.idGroup);
-            done(ResponseJSON(200, 'Successfull', data));
+            doNotification(data.idGroup, rs, "remove");
+            done(ResponseJSON(200, 'Successfully', data));
         }).catch(err => {
             done(ResponseJSON(512, err, err));
         });
     } else {
         done(ResponseJSON(512, "Type must be 'add' or 'remove'"));
     }
-
-
 }
 
 function getAllSharedProject(data, done) {
     Model.SharedProject.findAll({
         include: [
-            {model: Model.User},
-            {model: Model.Group, include: Model.Company}
+            { model: Model.User },
+            { model: Model.Group, include: Model.Company }
         ]
     }).then(sps => {
         done(ResponseJSON(200, "Done", sps));
@@ -125,8 +135,8 @@ function getAllSharedProject(data, done) {
 function getSharedProjectList(data, done) {
     let listPrj = [];
     Model.User.findOne({
-        where: {username: data.username},
-        include: {model: Model.Group, include: {model: Model.SharedProject}}
+        where: { username: data.username },
+        include: { model: Model.Group, include: { model: Model.SharedProject } }
     }).then(user => {
         asyncEach(user.groups, function (group, nextGroup) {
             asyncEach(group.shared_projects, function (sharedProject, nextProject) {
@@ -146,10 +156,57 @@ function getSharedProjectList(data, done) {
                 nextGroup();
             });
         }, function () {
-            done(ResponseJSON(200, "Successfull", listPrj));
+            done(ResponseJSON(200, "Successfully", listPrj));
         });
     });
 }
+
+
+
+function doNotification(idGroup, sharedProject, action) {
+    Model.Group.findByPk(idGroup, { include: { model: Model.User } }).then(group => {
+        group = group.toJSON()
+        //all users in groups
+        let users = group.users;
+        let owner = users.find(u => u.idUser = sharedProject.idOwner);
+        users.forEach(user => {
+            if (user.email && user.idUser !== sharedProject.idOwner) {
+                if (action === "add") {
+                    console.log(`Send shared notification to email ${user.email} for project ${sharedProject.project_name}`)
+                    console.log(__dirname)
+                    //let str = `Hi <b>${user.username}</b>,<br><br>Your group <b>${group.name}</b> has been shared new project named <b>${sharedProject.project_name}</b> by <b>${owner.username}</b><br/><p>If you have any question please contact us via email: <a href="mailto:support@i2g.cloud"> support@i2g.cloud></a></p><br><p>Best regards</p></br><p>The I2G Support team</p>`
+                    let str = fs.readFileSync(__dirname + '/mail-template/add-share.html').toString()
+                    str = str.replace("__USER__", user.username);
+                    str = str.replace("__SHARED_USER__", owner.username);
+                    str = str.replace("__PROJECT_NAME__", sharedProject.project_name);
+                    str = str.replace("__WORKSPACE_URL__", process.env.WORKSPACE_URL ? process.env.WORKSPACE_URL : "https://wi.i2g.cloud")
+                    notice && notice.sendMail({
+                        to: user.email,
+                        messageHtml: str,
+                        subject: `[I2G Notification] ${owner.username} shared project ${sharedProject.project_name} with you!`
+                    });
+                } else if (action === "remove") {
+                    console.log(`Send un-shared notification to email ${user.email} for project ${sharedProject.project_name}`)
+                    // let str = `Hi <b>${user.username}</b>,<br><br>Your group <b>${group.name}</b> has been removed the project named <b>${sharedProject.project_name}</b> by <b>${owner.username}</b><br/><p>If you have any question please contact us via email: <a href="mailto:support@i2g.cloud"> support@i2g.cloud></a></p><br><p>Best regards</p></br><p>The I2G Support team</p>`
+                    let str = fs.readFileSync(__dirname + '/mail-template/remove-share.html').toString()
+                    str = str.replace("__USER__", user.username);
+                    str = str.replace("__SHARED_USER__", owner.username);
+                    str = str.replace("__PROJECT_NAME__", sharedProject.project_name);
+                    str = str.replace("__WORKSPACE_URL__", process.env.WORKSPACE_URL ? process.env.WORKSPACE_URL : "https://wi.i2g.cloud")
+                    notice && notice.sendMail({
+                        to: user.email,
+                        messageHtml: str,
+                        subject: `[I2G Notification] ${owner.username} stop sharing project ${sharedProject.project_name} with you!`
+                    });
+                } else {
+                    console.log("Send notification error, do not recognize action")
+                }
+
+            }
+        })
+    })
+}
+
 
 module.exports = {
     createNewSharedProject: createNewSharedProject,
